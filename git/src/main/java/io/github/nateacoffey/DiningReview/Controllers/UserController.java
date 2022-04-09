3d2081a79763af307ae149f6d6d8b853b5d0a2b2
Nathan <NateACoffey@gmail.com>
2022-04-09 12:50:27 -0700

    Added REST for searching by username, creating user, and updating existing user

--- git/src/main/java/io/github/nateacoffey/DiningReview/Controllers/UserController.java
+++ git/src/main/java/io/github/nateacoffey/DiningReview/Controllers/UserController.java
@@ -1,8 +1,17 @@
 package io.github.nateacoffey.DiningReview.Controllers;
 
 
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.nateacoffey.DiningReview.Entities.User;
 import io.github.nateacoffey.DiningReview.Repositories.UserRepository;
 
 
@@ -14,6 +23,59 @@ public class UserController {
 	public UserController(final UserRepository userRepository) {
 		this.userRepository = userRepository;
 	}
+	
+	
+	@GetMapping("/user/search")
+	public Iterable<User> findUserByUsername(@RequestParam("displayName") String displayName) {
+		return userRepository.findByDisplayName(displayName);
+	}
+	
+	@PostMapping("/user/add")
+	public User createNewUser(@RequestBody User user) {
+		
+		User newUser = userRepository.save(user);
+		
+		return newUser;
+		
+	}
+	
+	@PutMapping("/user/update/{id}")
+	public User updateUser(@PathVariable("id") Long id, @RequestBody User user) {
+		
+		Optional<User> optionalUser = userRepository.findById(id);
+		
+		if(!optionalUser.isPresent()) {
+			return null;
+		}
+		
+		User userToUpdate = optionalUser.get();
+		
+		if(user.getDairyAllergies() != null) {
+			userToUpdate.setDairyAllergies(user.getDairyAllergies());
+		}
+		if(user.getEggAllergies() != null) {
+			userToUpdate.setEggAllergies(user.getEggAllergies());
+		}
+		if(user.getPeanutAllergies() != null) {
+			userToUpdate.setPeanutAllergies(user.getPeanutAllergies());
+		}
+		if(user.getCity() != null) {
+			userToUpdate.setCity(user.getCity());
+		}
+		if(user.getState() != null) {
+			userToUpdate.setState(user.getState());
+		}
+		if(user.getZipCode() != null) {
+			userToUpdate.setZipCode(user.getZipCode());
+		}
+		
+		
+		User updatedUser = userRepository.save(userToUpdate);
+		
+		
+		return updatedUser;
+	}
+	
 	
 	
 }
--- git/src/main/java/io/github/nateacoffey/DiningReview/Repositories/UserRepository.java
+++ git/src/main/java/io/github/nateacoffey/DiningReview/Repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package io.github.nateacoffey.DiningReview.Repositories;
 
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import io.github.nateacoffey.DiningReview.Entities.User;
@@ -8,4 +10,5 @@ import io.github.nateacoffey.DiningReview.Entities.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
+	List<User> findByDisplayName(String displayName);
 }
