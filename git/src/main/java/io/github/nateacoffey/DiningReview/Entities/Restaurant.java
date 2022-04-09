68517414862f961b31d21b546bf5150a5e4767c6
Nathan <NateACoffey@gmail.com>
2022-04-08 23:27:47 -0700

    Defined restaurant entity

--- git/src/main/java/io/github/nateacoffey/DiningReview/Entities/Restaurant.java
+++ git/src/main/java/io/github/nateacoffey/DiningReview/Entities/Restaurant.java
@@ -1,10 +1,38 @@
 package io.github.nateacoffey.DiningReview.Entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "RESTAURANTS")
 public class Restaurant {
-
+	
+	@Id
+	@GeneratedValue
+	@Getter
+	private Long id;
+	
+	@Getter @Setter
+	@Column(name = "OVERALL_SCORE")
+	private Integer overallScore;
+	
+	@Getter @Setter
+	@Column(name = "PEANUT_ALLERGY")
+	private Integer peanutAllergy;
+	
+	@Getter @Setter
+	@Column(name = "EGG_ALLERGY")
+	private Integer eggAllergy;
+	
+	@Getter @Setter
+	@Column(name = "DAIRY_ALLERGY")
+	private Integer dairyAllergy;
+	
+	
 }
