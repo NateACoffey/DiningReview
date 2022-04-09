32f334504545330c62b45233cd6c6ea0beede041
Nathan <NateACoffey@gmail.com>
2022-04-08 23:06:36 -0700

    Created entities

---
+++ git/src/main/java/io/github/nateacoffey/DiningReview/Entities/User.java
@@ -0,0 +1,10 @@
+package io.github.nateacoffey.DiningReview.Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USERS")
+public class User {
+
+}
