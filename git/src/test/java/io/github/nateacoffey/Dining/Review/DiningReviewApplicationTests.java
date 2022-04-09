70d3cc99bf198b913091e3d2b9c3d746b3d63356
Nathan <NateACoffey@gmail.com>
2022-04-08 22:32:52 -0700

    First commit

---
+++ git/src/test/java/io/github/nateacoffey/Dining/Review/DiningReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nateacoffey.Dining.Review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
