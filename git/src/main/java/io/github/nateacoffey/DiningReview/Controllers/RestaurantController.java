f6701de00111ec5b5c795fb1a8e7c3b74a7d305a
Nathan <NateACoffey@gmail.com>
2022-04-13 20:32:35 -0700

    Created REST for creating and searching restaurants

--- git/src/main/java/io/github/nateacoffey/DiningReview/Controllers/RestaurantController.java
+++ git/src/main/java/io/github/nateacoffey/DiningReview/Controllers/RestaurantController.java
@@ -1,8 +1,16 @@
 package io.github.nateacoffey.DiningReview.Controllers;
 
 
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.nateacoffey.DiningReview.Entities.Restaurant;
 import io.github.nateacoffey.DiningReview.Repositories.RestaurantRepository;
 
 
@@ -14,5 +22,72 @@ public class RestaurantController {
 	public RestaurantController(final RestaurantRepository restaurantRepository) {
 		this.restaurantRepository = restaurantRepository;
 	}
+	
+	
+	@GetMapping("/restaurant/{id}")
+	public Restaurant getRestaurantById(@PathVariable("id") Long id) {
+		
+		Optional<Restaurant> restaurantOptional = restaurantRepository.findById(id);
+		
+		if(!restaurantOptional.isPresent()) {
+			return null;
+		}
+		
+		Restaurant restaurant = restaurantOptional.get();
+		
+		return restaurant;
+	}
+	
+	@GetMapping("/restaurant/search")
+	public Iterable<Restaurant> getRestaurantByZipCode(
+			@RequestParam("zipCode") Integer zipCode, 
+			@RequestParam(name = "peanutAllergy", required = false) Integer peanutAllergy, 
+			@RequestParam(name = "eggAllergy", required = false) Integer eggAllergy, 
+			@RequestParam(name = "dairyAllergy", required = false) Integer dairyAllergy) {
+		
+		
+		if(peanutAllergy == null && eggAllergy == null && dairyAllergy == null) {
+			Iterable<Restaurant> restaurant = restaurantRepository.findByZipCode(zipCode);
+			
+			return restaurant;
+		}
+		
+		if(peanutAllergy != null) {
+			Iterable<Restaurant> restaurant = restaurantRepository.findByZipCodeAndPeanutAllergyIsNotNullOrderByPeanutAllergyDesc(zipCode);
+			
+			return restaurant;
+		}
+		
+		if(eggAllergy != null) {
+			Iterable<Restaurant> restaurant = restaurantRepository.findByZipCodeAndEggAllergyIsNotNullOrderByEggAllergyDesc(zipCode);
+			
+			return restaurant;
+		}
+		
+		if(dairyAllergy != null) {
+			Iterable<Restaurant> restaurant = restaurantRepository.findByZipCodeAndDairyAllergyIsNotNullOrderByDairyAllergyDesc(zipCode);
+			
+			return restaurant;
+		}
+		
+		
+		return null;
+	}
+	
+	@PostMapping("/restaurant/create")
+	public Restaurant createRestaurant(@RequestBody Restaurant restaurant) {
+		
+		
+		Iterable<Restaurant> nameAndZipCodeRestaurant = restaurantRepository.findByNameAndZipCode(restaurant.getName(), restaurant.getZipCode());
+		
+		if(nameAndZipCodeRestaurant.iterator().hasNext()) {
+			return null;
+		}
+		
+		Restaurant newRestaurant = restaurantRepository.save(restaurant);
+		
+		return newRestaurant;
+	}
+	
 	
 }
--- git/src/main/java/io/github/nateacoffey/DiningReview/Repositories/RestaurantRepository.java
+++ git/src/main/java/io/github/nateacoffey/DiningReview/Repositories/RestaurantRepository.java
@@ -7,5 +7,16 @@ import io.github.nateacoffey.DiningReview.Entities.Restaurant;
 
 
 public interface RestaurantRepository extends CrudRepository<Restaurant, Long> {
-
+	
+	Iterable<Restaurant> findByNameAndZipCode(String name, Integer zipCode);
+	
+	Iterable<Restaurant> findByZipCode(Integer zipCode);
+	
+	Iterable<Restaurant> findByZipCodeAndPeanutAllergyIsNotNullOrderByPeanutAllergyDesc(Integer zipCode);
+	
+	Iterable<Restaurant> findByZipCodeAndEggAllergyIsNotNullOrderByEggAllergyDesc(Integer zipCode);
+	
+	Iterable<Restaurant> findByZipCodeAndDairyAllergyIsNotNullOrderByDairyAllergyDesc(Integer zipCode);
+	
+	
 }
